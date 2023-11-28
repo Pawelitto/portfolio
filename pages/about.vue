7480fb96464ed587a6284b17f38f45c0d17c30cf
Paweł Pietras <pawel.pietras144@gmail.com>
2023-11-28 11:37:14 +0100

    15. Setting Diffrent Layouts

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>This page is about me.</div>
+  <!-- <div>
+    <button @click="enableCustomLayout">Update layout</button>
+  </div> -->
+</template>
+<script setup>
+// function enableCustomLayout() {
+//   setPageLayout("another");
+// }
+</script>
