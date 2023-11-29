f8ce875e3b4be9ed7e4ceea7067a029633fa275c
Paweł Pietras <pawel.pietras144@gmail.com>
2023-11-29 09:29:33 +0100

    23. Dark Mode Selector Button

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="container mx-auto max-w-2xl">
     <header class="flex justify-between items-center mt-5">
-      <div>
-        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
-          >Paweł Pietras</NuxtLink
-        >
-      </div>
+      <div class="flex items-center space-x-12">
+        <div>
+          <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
+            >Paweł Pietras</NuxtLink
+          >
+        </div>
 
-      <Menu />
+        <Menu />
+      </div>
+      <ClientOnly>
+        <ColorModeSelector />
+      </ClientOnly>
     </header>
     <main class="p-2 mt-10">
       <slot />
@@ -33,5 +38,9 @@ useHead({
 <style>
 body {
   font-family: roboto;
+}
+
+body {
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300;
 }
 </style>
